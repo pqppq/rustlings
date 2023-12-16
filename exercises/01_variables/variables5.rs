b4726ecbe4a6477c8211aed5223ce8dd78db0394
pqppq <yutatsu0119@gmail.com>
2023-12-16 18:12:30 +0900

    variables

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -3,11 +3,10 @@
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+// rustはshadowingをサポートしていて、別の型で同じ名前の変数を再定義できる
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
+    let number = 3; // don't rename this variable
     println!("Number plus two is : {}", number + 2);
 }
