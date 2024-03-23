2a807669853864110aa60a2c8126a7a3bbdda269
pqppq <yutatsu0119@gmail.com>
2024-03-23 14:27:36 +0900

    errors

--- exercises/13_error_handling/errors5.rs
+++ exercises/13_error_handling/errors5.rs
@@ -22,14 +22,14 @@
 // Execute `rustlings hint errors5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::error;
 use std::fmt;
-use std::num::ParseIntError;
 
 // TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), Box<dyn ???>> {
+// https://doc.rust-jp.rs/rust-by-example-ja/std/box.html
+// Boxとはヒープ上のデータを指すスマートポインタ
+// Err -> ParseIneError or CreationError
+fn main() -> Result<(), Box<dyn error::Error>> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
